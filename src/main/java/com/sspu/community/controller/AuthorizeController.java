1c3174022531d8de383c1409feb400bc9e75ce76
Aerithhh <finchhhh@gmail.com>
2022-03-24 22:50:16 +0800

    data

--- src/main/java/com/sspu/community/controller/AuthorizeController.java
+++ src/main/java/com/sspu/community/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author Sun
  * @date 2022/3/23 23:09
@@ -26,7 +28,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                            HttpServletRequest request){
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
@@ -36,7 +39,12 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GitHubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if(user !=null){
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";
+        }else{
+            return "redirect:/";
+        }
+
     }
 }
