00c957c79d6bf097bf0f9b80455dc3bba06ea0e3
Aerithhh <finchhhh@gmail.com>
2022-03-25 02:53:07 +0800

    qwq

---
+++ src/main/java/com/sspu/community/mapper/UserMapper.java
@@ -0,0 +1,5 @@
+package com.sspu.community.mapper;/**
+ * @author Sun
+ * @date 2022/3/25 0:51
+ */public class UserMapper {
+}
---
+++ src/main/java/com/sspu/community/model/User.java
@@ -0,0 +1,5 @@
+package com.sspu.community.model;/**
+ * @author Sun
+ * @date 2022/3/25 1:13
+ */public class User {
+}
